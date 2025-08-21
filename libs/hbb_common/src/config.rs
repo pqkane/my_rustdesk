05c062c5b7e815af3f51bceec9209e6783bc7e61
pqkane <pqkane@qq.com>
2025-08-21 17:51:15 +0800

    config.rs
    更改中继服务器地址

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["21116.xsgai.top"];
-pub const RELAY_SERVERS: &[&str] = &["21117.xsgai.top"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["21117.xsgai.top"];
+//pub const RELAY_SERVERS: &[&str] = &["21117.xsgai.top"];
 pub const RS_PUB_KEY: &str = "Dx0BFxcvXOq4qHwKDn1z2po6INrqLJhQg6lCXMrgT88=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
